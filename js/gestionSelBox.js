67cd84363e3ce463810c68375f108e19b3bb2fd4
maikelreyy <104846653+maikelreyy@users.noreply.github.com>
2022-06-26 20:19:16 +0200

    Update gestionSelBox.js

--- js/gestionSelBox.js
+++ js/gestionSelBox.js
@@ -10,55 +10,42 @@ function selBoxGestion() {
     const src = `./img/${op}.png`;
     switch (op) {
       case "":
-        console.log(op);
         document.getElementById("img").src = src;
         break;
       case "Nev":
-        console.log(op);
         document.getElementById("img").src = src;
         break;
       case "Lav":
-        console.log(op);
         document.getElementById("img").src = src;
         break;
       case "Vit":
-        console.log(op);
         document.getElementById("img").src = src;
         break;
       case "Vaj":
-        console.log(op);
         document.getElementById("img").src = src;
         break;
       case "Caf":
-        console.log(op);
         document.getElementById("img").src = src;
         break;
       case "TV":
-        console.log(op);
         document.getElementById("img").src = src;
         break;
       case "PC":
-        console.log(op);
         document.getElementById("img").src = src;
         break;
       case "SisSon":
-        console.log(op);
         document.getElementById("img").src = src;
         break;
       case "Carg":
-        console.log(op);
         document.getElementById("img").src = src;
         break;
       case "Asp":
-        console.log(op);
         document.getElementById("img").src = src;
         break;
       case "Pla":
-        console.log(op);
         document.getElementById("img").src = src;
         break;
       case "Otr":
-        console.log(op);
         document.getElementById("img").src = src;
         break;
     }
